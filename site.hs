f1771b6e0b9a42433907b026c5aa723f142afaab
Adam Fiedler <adam@fiedler.sk>
2019-02-25 00:32:09 +0100

    Add rest of the template

--- site.hs
+++ site.hs
@@ -57,19 +57,13 @@ pandocBiblioCompilerWith ropt wopt cslFileName bibFileName = do
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match (fromList [".htaccess", "browserconfig.xml", "favicon.ico", "images/*"])  $ do
         route   idRoute
         compile copyFileCompiler
 
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
-
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
 
     match "csl/*" $ compile cslCompiler
     match "bib/*" $ compile biblioCompiler
